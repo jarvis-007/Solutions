673cba0a82fc8505ba40c70d8bf9ec89b8cb390a
jarvis-007 <47780212+jarvis-007@users.noreply.github.com>
2020-06-18 18:54:03 +0530

    From Codeforces
    https://codeforces.com/contest/1367/problem/B

--- Even_Array.java
+++ Even_Array.java
@@ -19,7 +19,7 @@ public static void main(String[] args) {
         int odd_mismatch=0;
         for(int i=0;i<n;i++)
             nums[i]=sc.nextInt();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++) 
         {
             if(i%2==0)
             {
